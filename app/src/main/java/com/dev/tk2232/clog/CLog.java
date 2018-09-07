bca000d179b68fc8dabed5b203b2e5eefe5c1991
Sebastian Esau <s.esau89@gmail.com>
2018-09-07 10:57:30 +0200

    Initial commit

---
+++ app/src/main/java/com/dev/tk2232/clog/CLog.java
@@ -0,0 +1,68 @@
+package com.dev.tk2232.clog;
+
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * Custom Log class
+ * Falls die Fehler an den Server geschickt werden sollen dann können die Methoden hier hinzugefügt werden.
+ */
+
+public class CLog {
+
+    private static Configuration conf = new Configuration();
+
+    public static void e(String tag, String msg) {
+        if (conf.logWithConsole)
+            Log.e(tag, msg);
+
+        if (conf.logWithFirebase) {
+            //TODO log with firebase
+        }
+    }
+
+    public static void e(String tag, String msg, Throwable tr) {
+        if (conf.logWithConsole)
+            Log.e(tag, msg, tr);
+
+        if (conf.logWithFirebase) {
+            //TODO log with firebase
+        }
+    }
+
+    public static Configuration configuration () {
+        conf = new Configuration();
+        return conf;
+    }
+
+    public static Configuration configuration (Context context) {
+        conf = new Configuration();
+        return conf;
+    }
+
+    public static class Configuration {
+
+        private Context context;
+
+        private boolean logWithFirebase = false;
+        private boolean logWithConsole = false;
+
+        public Configuration() {
+
+        }
+
+        public Configuration(Context context) {
+            this.context = context;
+        }
+
+        public Configuration logWithFirebase(boolean logWithFirebase) {
+            this.logWithFirebase = logWithFirebase;
+            return this;
+        }
+
+        public Configuration logWithConsole(boolean logWithConsole) {
+            this.logWithConsole = logWithConsole;
+            return this;
+        }
+    }
+}
